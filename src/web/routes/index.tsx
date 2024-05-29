1997eb4a1d309cc17b092f64f9762028592e9e50
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-05-29 14:03:25 +0100

    UI change for a new style

--- src/web/routes/index.tsx
+++ src/web/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@radix-ui/themes";
+import { Box, Flex, Text } from "@radix-ui/themes";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -6,5 +6,14 @@ export const Route = createFileRoute("/")({
 });
 
 function Index() {
-  return <Flex grow="1" direction="column" gap="4" p="2"></Flex>;
+  return (
+    <Flex grow="1" className="items-center justify-center">
+      <Flex direction="column" align="center" className="space-y-2">
+        <Box className="w-10 h-10 rounded-full shadow-md cursor-pointer border-1 border-solid border-zinc-400 dark:border-zinc-800" />
+        <Text size="3" color="gray">
+          You
+        </Text>
+      </Flex>
+    </Flex>
+  );
 }
