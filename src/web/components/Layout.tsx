ecc92c4d21e2f224e6848b596a39bd61263347e7
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-06-05 16:32:33 +0100

    [chore]cleanuo

--- src/web/components/Layout.tsx
+++ src/web/components/Layout.tsx
@@ -3,8 +3,8 @@ import { useObserveEffect } from "@legendapp/state/react";
 import { Button, Flex } from "@radix-ui/themes";
 import { fileTransferState$, globalState$ } from "@shared/state";
 import t from "@src/shared/config";
-import { Link, useRouterState } from "@tanstack/react-router";
-import { ChevronLeft, Minus, Moon, Plus, Sun, X } from "lucide-react";
+import { useRouterState } from "@tanstack/react-router";
+import { Minus, Moon, Plus, Sun, X } from "lucide-react";
 import type React from "react";
 import { useCallback } from "react";
 
@@ -17,8 +17,6 @@ export default function Layout({ children }: LayoutProps) {
   const { mutate: closeWindow } = t.window.closeWindow.useMutation();
 
   const navState = useRouterState();
-
-  const isHome = navState.location.pathname === "/";
 
   const isDarkMode = computed(() => globalState$.colorMode.get() === "dark");
 
@@ -59,17 +57,6 @@ export default function Layout({ children }: LayoutProps) {
         gap="4"
         className="absolute z-10 w-full px-4 py-3 top-0 left-0"
       >
-        {!isHome && (
-          <Button
-            variant="ghost"
-            className="w-2.5 h-4.5 rounded-full cursor-pointer"
-            asChild
-          >
-            <Link to="/">
-              <ChevronLeft size={12} />
-            </Link>
-          </Button>
-        )}
         <Flex grow="1" id="drag-region" className="p-2" />
         <Flex align="center" justify="end" gap="5">
           <Button
