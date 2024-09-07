ee6d9ccbb0a5ef100c0bf73bda72c41cafeb6493
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-09-07 16:30:54 +0100

    starting to launch file-server from layout

--- src/shared/routers/node.ts
+++ src/shared/routers/node.ts
@@ -2,7 +2,6 @@ import { publicProcedure, router } from "@src/trpc";
 import startServer from "@shared/core/file-server";
 import {globalState$} from "@shared/state";
 
-// TODO
 export const nodeRouter = router({
     startServer:publicProcedure.mutation(async()=>startServer(globalState$.port.get())),
 });
